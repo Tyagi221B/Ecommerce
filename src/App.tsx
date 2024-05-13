928e7c17c70e5f757feea8954dfb70c7f71a1c7d
Asmit tyagi <ashmittyagigms@gmail.com>
2024-05-13 14:08:58 +0530

    add mixin.scss, add other pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import Loader from "./components/Loader"
 const Home = lazy(()=> import("./pages/Home"))
 const Search = lazy(()=> import("./pages/Search"))
 const Cart = lazy(()=> import("./pages/Cart"))
+const Dashboard = lazy(()=> import("./pages/Dashboard"))
+const Products = lazy(()=> import("./pages/Products"))
+const Customers = lazy(()=> import("./pages/Customers"))
+const Transaction = lazy(()=> import("./pages/Transaction"))
 
 
 const App = () => {
@@ -13,6 +17,10 @@ const App = () => {
       {/* Header  */}
       <Suspense fallback={<Loader/>}>
       <Routes>
+        <Route path="/admin/dashboard" element={<Dashboard/>}/>
+        <Route path="/admin/products" element={<Products/>}/>
+        <Route path="/admin/Customers" element={<Customers/>}/>
+        <Route path="/admin/transaction" element={<Transaction/>}/>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/cart" element={<Cart />} />
